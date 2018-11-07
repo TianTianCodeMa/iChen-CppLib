9c8fffa187ca89ca0acde6ae0b4fda334fa3ecd1
Stephen Chung <Stephen.Chung@intexact.com>
2018-11-07 14:25:10 +0800

    Initial release.

---
+++ LibSmokeTest/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef _CRT_RAND_S
+#define _CRT_RAND_S
+#endif // !_CRT_RAND_S
+
+// TODO: reference additional headers your program requires here
+#include <Windows.h>
+#include <stdlib.h>
---
+++ README.md
@@ -0,0 +1,7 @@
+# iChen® 4.0 C++ Integration Library
+
+C++ library to connect to the iChen&reg; System using the iChen protocol.
+
+API documentation can be found here: [`ichen-cpp-lib-reference.md`](https://github.com/chenhsong/iChen-CppLib/blob/master/ichen-cpp-lib-reference.md)
+
+`LibSmokeTest` is a simple test program.
